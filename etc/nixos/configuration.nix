eb31a65fa560afa50c9a8ea5bbeb52defe6b7eaf
tyler <tyler>
2026-09-10 01:42:37 -0400

    Random MAC addr added to config

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -18,6 +18,7 @@
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.macAddress = "random";
   networking.firewall.enable = true;
 
   # ============================================================================
