8d9d2b1194c7a72ed884dce21562bcd23c7a1512
tyler <tyler>
2026-09-16 04:47:45 -0400

    qt software needed a variable to point to gtk file opener

--- modules/de/gnome.nix
+++ modules/de/gnome.nix
@@ -2,12 +2,16 @@
 # Desktop / Display
 # ============================================================================
 
-{...}: 
+{pkgs, ...}: 
 
 { 
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
   services.gvfs.enable = true;
+
+  environment.extraInit = ''
+    export XDG_DATA_DIRS="$XDG_DATA_DIRS:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+  '';
 
   services.xserver.xkb = {
     layout = "us";
