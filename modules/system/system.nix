1e051e572e1baee0e25cec7421342460150637db
tyler <tyler>
2026-09-18 02:04:57 -0400

    finishing up sway setup

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -8,8 +8,20 @@
 
   services.libinput.enable = true;
   services.flatpak.enable = true;
-
   programs.firefox.enable = true;
+
+  programs.uwsm = {
+    enable = true;
+    # You must configure the waylandCompositors suboptions so that UWSM knows which compositors to manage. 
+    waylandCompositors = {
+      sway = {
+        prettyName = "Sway";
+        comment = "Sway compositor managed by UWSM";
+        binPath = "/run/current-system/sw/bin/sway";
+      };
+    };
+  };
+  
   # ============================================================================
   # Nix
   # ============================================================================
