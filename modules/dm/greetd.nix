14dda9a82e57bf65eb32f6af05674983b00b6379
Tyler Pittman <45609608+TPi-Home@users.noreply.github.com>
2026-09-18 03:25:52 -0400

    Use SwayFX for ReGreet compositor

--- modules/dm/greetd.nix
+++ modules/dm/greetd.nix
@@ -31,7 +31,7 @@
 
     settings = {
       default_session = {
-        command = "${pkgs.sway}/bin/sway --unsupported-gpu --config /etc/greetd/sway-config";
+        command = "${pkgs.swayfx}/bin/sway --unsupported-gpu --config /etc/greetd/sway-config";
         user = "greeter";
       };
     };
@@ -44,6 +44,6 @@
   environment.etc."greetd/sway-config".text = ''
     set $SWAYSOCK /run/user/$(id -u)/sway-ipc.sock
 
-    exec "${pkgs.regreet}/bin/regreet; ${pkgs.sway}/bin/swaymsg exit"
+    exec "${pkgs.regreet}/bin/regreet; ${pkgs.swayfx}/bin/swaymsg exit"
   '';
 }
