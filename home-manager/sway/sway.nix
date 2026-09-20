c61f9746a0ed27eac7340b5d45cb0bdfa6d13a72
tyler <tyler>
2026-09-20 13:53:44 -0400

    updated screenshot and cleaning up confs

--- home-manager/sway/sway.nix
+++ home-manager/sway/sway.nix
@@ -3,6 +3,17 @@
 {
   xdg.configFile."sway/config".source = ../../home/.config/sway/config;
 
+  xdg.configFile."swaynag/config".text = ''
+    [logout]
+    background=161718
+    border=85befd
+    border-bottom=85befd
+    button-background=85befd
+    text=c4c8c5
+    button-text=161718
+    edge=bottom
+  '';
+
   home.pointerCursor = {
     enable = true;
     package = pkgs.adwaita-icon-theme;
