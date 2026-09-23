54bc6b290ef585d7651f0eeeefee599a789a0373
tyler <tyler>
2026-09-23 06:39:33 -0400

    breaking things now that i got them working

--- home-manager/pkg/dconf.nix
+++ home-manager/pkg/dconf.nix
@@ -4,6 +4,14 @@
   gtk = {
     enable = true;
 
+    gtk3 = {
+      enable = true;
+      colorScheme = "dark";
+      theme = {
+        name = "Adwaita-dark";
+      };
+    };
+
     gtk4 = {
       enable = true;
       colorScheme = "dark";
@@ -13,6 +21,7 @@
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
+      gtk-theme = "Adwaita-dark";
     };
   };
 }
