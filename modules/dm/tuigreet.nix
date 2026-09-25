e6683b236442985e006be2b7a6e4924c3d8fd441
tyler <tyler>
2026-09-25 15:02:01 -0400

    breaking things now that i got them working

--- modules/dm/tuigreet.nix
+++ modules/dm/tuigreet.nix
@@ -34,6 +34,18 @@
     };
   };
 
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal";
+
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+  
   environment.systemPackages = with pkgs; 
   [
     tuigreet
